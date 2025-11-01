d54c3164adba2a2aca285222f6b443bd68447a85
Abhishek Mahajan <abhishekmahajan255@gmail.com>
2025-11-01 11:49:00 +0530

    Initial code setup commit

---
+++ claimprocessingservice/src/main/java/com/claimprocessing/claimprocessingservice/ClaimProcessingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.claimprocessing.claimprocessingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimProcessingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimProcessingServiceApplication.class, args);
+	}
+
+}
---
+++ claimprocessingservice/src/test/java/com/claimprocessing/claimprocessingservice/ClaimProcessingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claimprocessing.claimprocessingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimProcessingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
